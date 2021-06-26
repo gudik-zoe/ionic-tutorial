44ac6e9a197c7127ee59662ca64913b354fa7170
antoine khoury <antoine.khoury@scaiconsulting.it>
2021-06-26 17:19:25 +0200

    styling the offers page using css variables

--- src/app/auth/auth-service.service.ts
+++ src/app/auth/auth-service.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AuthServiceService {
-  isAuth: boolean = false;
+  isAuth: boolean = true;
 
   constructor() {}
 
